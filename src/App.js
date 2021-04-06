c40420dacaa25db0367d22b76b2486b184998de2
phamhoc-99 <hoanghoatham1999@gmail.com>
2021-04-06 13:30:26 +0700

    doing random

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Counter from './components/Counter';
+import RandomQuotes from './components/RandomQuotes';
 class App extends Component {
 
  render() {
     return (
       <Counter/>
+      <RandomQuotes/>
     );
   }
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -7,7 +7,6 @@ class Counter extends React.Component {
     };
   }
 
-
   increment() {
     this.setState({
       count: this.state.count + 1
@@ -30,7 +29,7 @@ class Counter extends React.Component {
     return (
 
 <div className="bg-red-100 w-full h-screen  flex items-center">
-   <div className="bg-green-100 w-3/12 h-48 text-center  mx-auto pt-12" >
+   <div className="bg-green-100 w-full h-48 text-center  mx-auto pt-12 sm:w-3/12" >
    <button className='inc bg-green-500 rounded-md mx-3 w-9' onClick={(e) => this.increment(e)}>+</button>
     <button className='dec bg-green-500 rounded-md mx-3 w-9' onClick={(e) => this.decrement(e)}>-</button>
     <button className='reset bg-green-500 rounded-md mx-3' onClick={(e) => this.reset(e)}>Reset</button>
